7e56c6ddf1e73823990f3f010afcf4c1f7799887
Emmanuel Enya <emmanuelenya@meltwater.org>
2022-10-22 16:34:04 +0000

    implement odd numbers with while and for loop

---
+++ odd_numbers.py
@@ -0,0 +1,15 @@
+# print out odd numbers with for and while loop from 1 - 30
+
+
+print("Odd numbers from 1 - 30 using for loop: ")
+for i in range(1, 30, 2):
+    print(i)
+
+
+
+print("Odd numbers from 1 - 30 using while loop: ")
+
+count = 1
+while (count <= 30):
+    print(count)
+    count += 2
