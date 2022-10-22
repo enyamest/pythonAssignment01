45547726a911793c7424309a85e2051eb5dac759
Emmanuel Enya <emmanuelenya@meltwater.org>
2022-10-22 16:30:51 +0000

    implement even numbers with while and for loop

---
+++ even_numbers.py
@@ -0,0 +1,18 @@
+
+# print out even numbers with for and while loop from 1 - 30
+
+print("Even numbers from 1 - 30 using for loop: ")
+for i in range(2, 31, 2):
+    print(i)
+
+
+print("Even numbers from 1 - 30 using while loop: ")
+
+
+count = 0
+while (count <= 30):
+    print(count)
+    count += 2
+    
+
+
